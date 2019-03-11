8aef1fe65b60caa2461837978ba11fb822b74ffe
onHawk <luisd824@gmail.com>
2019-03-11 18:19:43 -0400

    how to navigate

--- demo/screens/Home.js
+++ demo/screens/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class HomeScreen extends Component {
   render() {
     return (
       <View>
         <Text>this is home HomeScreen</Text>
+        <Button
+          title="adds second screen to stack"
+          onPress={() => this.props.navigation.push('Home')}
+        >
+          Push Home
+        </Button>
       </View>
     );
   }
--- demo/screens/Something.js
+++ demo/screens/Something.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class Some extends Component {
   render() {
     return (
       <View>
         <Text>Another screen</Text>
+        <Text
+          title="secondscreen"
+          onPress={() => this.props.navigation.navigate('Home')}
+        >
+          Go to home screen
+        </Text>
       </View>
     );
   }
