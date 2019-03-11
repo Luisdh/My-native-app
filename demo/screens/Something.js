727fe99d7b49c025c30b88f03eea398074ebedf5
onHawk <luisd824@gmail.com>
2019-03-11 17:47:51 -0400

    create 2 simple screens

---
+++ demo/screens/Something.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Some extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Another screen</Text>
+      </View>
+    );
+  }
+}
